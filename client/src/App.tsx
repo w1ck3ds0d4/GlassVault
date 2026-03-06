17fdaf7fb9090ff6b66656d9d19355912a8ca111
w1ck3ds0d4 <daniel.svs@outlook.com>
2026-03-06 09:34:42 +0100

    (fix) add missing css import in app entry point

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,4 @@
+import "./App.css";
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, useAuth } from "./context/AuthContext";
